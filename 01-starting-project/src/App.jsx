0e8dde1bacff4a0f2d54bdf892fda98778c39441
하방방 <81242485+jumosd@users.noreply.github.com>
2024-03-04 07:07:45 +0900

    img를 넣는방법과 props를 사용하는법

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,22 +1,51 @@
+import reactImg from './assets/react-core-concepts.png'
+import componentsImage from './assets/components.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   )
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h1>{props.title}</h1>
+      <h3>{props.description}</h3>
+    </li>
+  )
+}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Compoenets"
+              description="The core UI building block"
+              image={componentsImage} />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
