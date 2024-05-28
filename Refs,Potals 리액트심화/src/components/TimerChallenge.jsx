130137bbec1f45d9567c9b71d01cdf0f1c9dff29
하방방 <81242485+jumosd@users.noreply.github.com>
2024-05-28 11:41:49 +0900

    타이머 시작기능

--- Refs,Potals 리액트심화/src/components/TimerChallenge.jsx
+++ Refs,Potals 리액트심화/src/components/TimerChallenge.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [timerExpired, setTimerExpired] = useState(false)
+
+    const handleStart = () => {
+        setTimerStarted(true)
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000)
+    }
     return (
         <section className='challenge'>
             <h2>{title}</h2>
+            {timerExpired && <p>실패 했어요!</p>}
             <p className='challengs-time'>
                 {targetTime}초
             </p>
             <p>
-                <button>시작</button>
+                <button onClick={handleStart}>
+                    {timerStarted ? '멈춤' : '시작'}
+                </button>
             </p>
 
-            <p className=''>
-                타이머를시작합니다/타이머가중단되었습니다.
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? "타이머를시작합니다." : "타이머가중단되었습니다."}
             </p>
 
         </section>
