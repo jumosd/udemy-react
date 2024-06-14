a57f180479bd932afd7efbd4622bff5b216768df
하방방 <81242485+jumosd@users.noreply.github.com>
2024-06-14 17:32:43 +0900

    시간이경과함에따라 점수를주는 기능

--- Refs,Potals 리액트심화/src/components/ResultModal.jsx
+++ Refs,Potals 리액트심화/src/components/ResultModal.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+const ResultModal = forwardRef(({ targetTime, remainingTime, onReset }, ref) => {
     const dialog = useRef()
-
+    const userLost = remainingTime <= 0
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100)
+    console.log(score)
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -15,10 +18,12 @@ const ResultModal = forwardRef(({ result, targetTime }, ref) => {
 
     return (
         <dialog ref={dialog} className='result-modal' >
-            <h2>{result}</h2>
+
+            {userLost && <h2>졌습니다</h2>}
+            {!userLost && <h2>점수는: {score}점</h2>}
             <p>선택한 시간은 <strong>{targetTime}초</strong></p>
-            <p>타이머를 멈췄을때 시간은 <strong>X 초 입니다!</strong></p>
-            <form method='dialog'>
+            <p>타이머를 멈췄을때 시간은 <strong>{formattedRemainingTime} 초 남았습니다!</strong></p>
+            <form method='dialog' onSubmit={onReset}>
                 <button>닫기</button>
             </form>
         </dialog>
--- Refs,Potals 리액트심화/src/components/TimerChallenge.jsx
+++ Refs,Potals 리액트심화/src/components/TimerChallenge.jsx
@@ -1,27 +1,39 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ResultModal from './ResultModal';
 
 
 const TimerChallenge = ({ title, targetTime }) => {
     const timer = useRef()
     const dialog = useRef()
-    const [timerStarted, setTimerStarted] = useState(false)
-    const [timerExpired, setTimerExpired] = useState(false)
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000)
 
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000
+
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current)
+        dialog.current.open()
+    }
     const handleStart = () => {
-        timer.current = setTimeout(() => {
-            setTimerExpired(true)
-            dialog.current.open()
-        }, targetTime * 1000)
-        setTimerStarted(true)
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10)
+        }, 10)
     }
     const handleStop = () => {
-        clearTimeout(timer.current)
+        clearInterval(timer.current)
+        dialog.current.open()
+    }
+    const handleReset = () => {
+        setTimeRemaining(targetTime * 1000)
     }
 
     return (
         <>
-            <ResultModal ref={dialog} result='졌음' targetTime={targetTime} />
+            <ResultModal
+                ref={dialog}
+                result='졌음'
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset} />
 
             <section className='challenge'>
                 <h2>{title}</h2>
@@ -29,13 +41,13 @@ const TimerChallenge = ({ title, targetTime }) => {
                     {targetTime}초
                 </p>
                 <p>
-                    <button onClick={timerStarted ? handleStop : handleStart}>
-                        {timerStarted ? '멈춤' : '시작'}
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? '멈춤' : '시작'}
                     </button>
                 </p>
 
-                <p className={timerStarted ? 'active' : undefined}>
-                    {timerStarted ? "타이머를시작합니다." : "타이머가중단되었습니다."}
+                <p className={timerIsActive ? 'active' : undefined}>
+                    {timerIsActive ? "타이머를시작합니다." : "타이머가중단되었습니다."}
                 </p>
 
             </section>
