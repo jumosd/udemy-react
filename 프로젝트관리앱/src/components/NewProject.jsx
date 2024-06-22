f2d2fe1469d1426c20f98d59321b58eb4d0375f4
하방방 <81242485+jumosd@users.noreply.github.com>
2024-06-22 15:31:22 +0900

    모달창 구현 디자인추가

--- 프로젝트관리앱/src/components/NewProject.jsx
+++ 프로젝트관리앱/src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react';
 import Input from './Input';
-
+import Modal from './Modal';
 const NewProject = ({ onAdd }) => {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -10,6 +12,14 @@ const NewProject = ({ onAdd }) => {
         const enteredTitle = title.current.value
         const enteredDescription = description.current.value
         const enteredDueDate = dueDate.current.value
+
+        if (enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === ''
+        ) {
+            modal.current.open()
+            return
+        }
 
         onAdd({
             title: enteredTitle,
@@ -20,6 +30,11 @@ const NewProject = ({ onAdd }) => {
 
     return (
         <div className='w-[35rem] mt-16'>
+            <Modal ref={modal} >
+                <h2 className='text-xl font-bold text-stone-700 my-4'>입력이 잘못 됐습니다</h2>
+                <p className='text-stone-600 mb-4'>제목과 설명 그리고 기간을 정해주세요</p>
+                <p className='text-stone-600 mb-4'>빈칸을 입력하면안됩니다 :D</p>
+            </Modal>
             <menu className='flex items-center justify-end gap-4 my-4'>
                 <li><button className='text-stone-800 hover:text-stone-950'>취소</button></li>
                 <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:text-tone-950'
