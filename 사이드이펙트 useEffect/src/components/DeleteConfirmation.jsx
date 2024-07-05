8dd8bcad5e06636f552f1dc98a07f9d71b737d69
하방방 <81242485+jumosd@users.noreply.github.com>
2024-07-05 11:51:56 +0900

    모달창 프로그레스바추가

--- 사이드이펙트 useEffect/src/components/DeleteConfirmation.jsx
+++ 사이드이펙트 useEffect/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER)
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 10)
+    }, 10)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   useEffect(() => {
     console.log("타이머시작")
     const timer = setTimeout(() => {
       onConfirm()
-    }, 3000)
+    }, TIMER)
 
 
     return () => {
@@ -24,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
