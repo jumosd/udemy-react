3b59c09c093822f2e8bf1d5d503dd7cdf261593b
하방방 <81242485+jumosd@users.noreply.github.com>
2024-07-26 12:35:09 +0900

    유저 장소 가져오기

--- HTTP요청 보내기/src/http.js
+++ HTTP요청 보내기/src/http.js
@@ -5,7 +5,19 @@ export const fetchingAvailablePlaces = async () => {
     const resData = await response.json()
     // 응답이 ok가아니면 에러를 만듭니다
     if (!response.ok) {
-        throw new Error("사진을 가져오지못했어요")
+        throw new Error("장소을 가져오지 못했어요")
+    }
+    return resData.places
+}
+
+export const fetchingUserPlaces = async () => {
+    // url에 get요청으로 유저 장소를 가져 옵니다
+    const response = await fetch("http://localhost:3000/user-places")
+    // 불러온데이터를 json 형식으로 변환합니다
+    const resData = await response.json()
+    // 응답이 ok가아니면 에러를 만듭니다
+    if (!response.ok) {
+        throw new Error("유저 장소를 가져오지 못했어요")
     }
     return resData.places
 }
